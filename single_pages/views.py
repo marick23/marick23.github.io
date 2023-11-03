cf2ea40078c45ba45e64ac5711ad51c071dd4c5e
Gayeon Kim <rkdus8567@naver.com>
2023-11-03 19:02:27 +0900

    네비바 수정

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(
+        request,
+        'single_pages/index.html',
+    )
+
+def company(request):
+    return render(
+        request,
+        'single_pages/company1.html'
+    )
