1ff36a384ca1727c9c15fddace6ad7f5af58f6fa
Gayeon Kim <rkdus8567@naver.com>
2023-11-07 20:35:15 +0900

    notice, single_pages 완성

--- doitdjango_prj/urls.py
+++ doitdjango_prj/urls.py
@@ -17,10 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('notice/', include('notice.urls')),
     path('', include('single_pages.urls')),
-
+    path('OOTD/', include('OOTD.urls')),
+    path('Event/', include('event.urls')),
+    path('Product/', include('product.urls')),
+    path('Detail/', include('product_detail.urls')),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- notice/models.py
+++ notice/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-
+import os
 class Notice(models.Model):
     title = models.CharField(max_length=50)
     cont = models.TextField()
+
+    head_image = models.ImageField(upload_to='notice/images/%Y/%m/%d', blank=True)
+    file_upload = models.FileField(upload_to='notice/files/%Y/%m/%d/', blank=True)
 
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
@@ -11,3 +14,12 @@ class Notice(models.Model):
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self):
+        return f'/notice/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
