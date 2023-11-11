0ad74370b90720de0bb155b0640c5ae7fc15a554
Gayeon Kim <rkdus8567@naver.com>
2023-11-12 02:15:13 +0900

    index 수정

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from event.models import EVENT
+from KLEAGUE.models import KLEAGUE
 def index(request):
     events = EVENT.objects.order_by('-created_at')[:3]
+    kleagues = KLEAGUE.objects.all()
     return render(
         request,
         'single_pages/index.html',
         {
-            'events': events
+            'events': events,
+            'KLEAGUES':kleagues
         }
     )
 
