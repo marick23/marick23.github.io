4ba32cfdb7aec014146bb238e3f39b9f41e3c60e
Gayeon Kim <rkdus8567@naver.com>
2023-11-13 00:18:10 +0900

    cart, payment 고치는중

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from KFA.models import KFA
+from KLEAGUE.models import KLEAGUE
 
 # Create your views here.
+
+def index(request):
+    products = KFA.objects.all()
+
+    return render(
+        request,
+        'cart/cart.html',
+        {
+            'pro' : products,
+        }
+    )
--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from KFA.models import KFA
 # Create your views here.
+
+def index(request):
+    products = KFA.objects.all()
+
+    return render(
+        request,
+        'payment/paymentpage.html',
+        {
+            'pro' : products,
+        }
+    )
