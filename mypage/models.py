c75d0a631e67ab593d42191b97099eb78c0a4cd9
Gayeon Kim <rkdus8567@naver.com>
2023-12-04 22:02:13 +0900

    수정중

--- mypage/models.py
+++ mypage/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Infor(models.Model):
-    name = models.TextField()
-    memberid = models.TextField()
-    pw = models.TextField()
-    phonenumber = models.TextField()
-    email = models.TextField()
-    address = models.TextField()
